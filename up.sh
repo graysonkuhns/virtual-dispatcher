7020486a9cea9368b49cad512b9cbebf12e27b9c
Grayson Kuhns <grayson.kuhns@xellitix.com>
2018-10-20 18:29:33 -0500

    Fix up script

--- up.sh
+++ up.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
+# Build the application Docker image
+docker build -t virtual-dispatcher:latest .
+
 # Stop the service
 docker stop virtual-dispatcher
 docker rm -f virtual-dispatcher
-
-# Build the application Docker image
-docker build -t virtual-dispatcher:latest .
 
 # Run the application
 docker run \
