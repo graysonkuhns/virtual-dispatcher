92f4abbb904353eb79cb01ee354d6fba18eeb5a5
Grayson Kuhns <grayson.kuhns@xellitix.com>
2018-10-19 23:41:41 -0500

    Add flight filtering based on completion

--- src/main/java/virtualdispatcher/resources/FlightsResource.java
+++ src/main/java/virtualdispatcher/resources/FlightsResource.java
@@ -2,11 +2,14 @@ package virtualdispatcher.resources;
 
 import com.codahale.metrics.annotation.Timed;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import virtualdispatcher.api.Flight;
 import virtualdispatcher.db.dao.FlightDAO;
@@ -31,7 +34,15 @@ public class FlightsResource implements Resource {
 
   @GET
   @Timed
-  public List<Flight> getFlights() {
-    return flightDAO.list();
+  public List<Flight> getFlights(
+      @QueryParam("aircraftId") final Integer aircraftId,
+      @QueryParam("completed") final Boolean completed) {
+
+    return flightDAO
+        .list()
+        .stream()
+        .filter(flight -> aircraftId == null || flight.getAircraftId() == aircraftId)
+        .filter(flight -> completed == null || flight.isCompleted() == completed)
+        .collect(Collectors.toList());
   }
 }
