26cd863858865308e245bf5287db627db8093a7a
jurichmitchell <jurichmitchell@gmail.com>
2018-10-19 20:40:21 -0500

    Update database/init.sql
    Naming updates and syntax changes

--- database/init.sql
+++ database/init.sql
@@ -6,32 +6,32 @@ CREATE DATABASE vd_db;
 USE vd_db;
 
 CREATE TABLE pilot (
-	pilot_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
 	f_name VARCHAR(30) NOT NULL,
 	l_name VARCHAR(30) NOT NULL,
 	PRIMARY KEY (pilot_id)
 );
 
 CREATE TABLE aircraft (
-	aircraft_id SMALLINT NOT NULL,
-	maintenence BOOL NOT NULL,
+	id SMALLINT NOT NULL,
+	operational BOOLEAN NOT NULL,
 	PRIMARY KEY (aircraft_id)
 );
 
 CREATE TABLE zones (
-	zone_id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (zone_id)
 );
 
 CREATE TABLE avaliability (
 	pilot_id SMALLINT UNSIGNED NOT NULL UNIQUE,
-	time_avaliable TIME NOT NULL,
+	created TIME NOT NULL,
 	PRIMARY KEY (pilot_id)
-)
+);
 
 CREATE TABLE flights (
-	flight_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	completed BOOL NOT NULL,
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	completed BOOLEAN NOT NULL,
 	aircraft_id SMALLINT NOT NULL,
 	PRIMARY KEY (flight_id)
 );
@@ -40,10 +40,10 @@ CREATE TABLE flights (
 
 ALTER TABLE avaliability
 	ADD FOREIGN KEY (pilot_id)
-	REFERENCES pilot (pilot_id)
+	REFERENCES pilot (id)
 	ON DELETE CASCADE;
 
 ALTER TABLE flights
-	ADD FOREIGN KEY (aircraft_id),
-	REFERENCES aircraft (aircraft_id),
+	ADD FOREIGN KEY (aircraft_id)
+	REFERENCES aircraft (id)
 	ON DELETE NO ACTION;
