e8ea5c7cf677a87e065c0e20c049bb9448d163f6
Grayson Kuhns <grayson.kuhns@xellitix.com>
2018-10-20 15:32:45 -0500

    Make service-up script more useful

--- up.sh
+++ up.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# Stop the service
+docker stop virtual-dispatcher
+docker rm -f virtual-dispatcher
+
+# Build the application Docker image
+docker build -t virtual-dispatcher:latest .
+
+# Run the application
 docker run \
   --name virtual-dispatcher \
   --network host \
