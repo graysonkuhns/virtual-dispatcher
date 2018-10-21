158c77ef3c83e946c5e5f432813d11391df6f15d
Grayson Kuhns <grayson.kuhns@xellitix.com>
2018-10-20 19:32:01 -0500

    fix flight scheduling

--- src/main/java/virtualdispatcher/db/dao/FlightDAO.java
+++ src/main/java/virtualdispatcher/db/dao/FlightDAO.java
@@ -59,13 +59,16 @@ public class FlightDAO {
 
   public void create(final Flight flight) {
     jdbi.useHandle(handle -> handle
-      .createQuery("INSERT INTO flights " +
-          "(completed, started, pilot_id, aircraft_id, zone_id) " +
-          "VALUES (:completed, :started, :pilot_id, :aircraft_id, :zone_id)")
+      .createUpdate(
+          "INSERT\n" +
+              "  INTO flights (completed, started, pilot_id, aircraft_id, zone_id)\n" +
+              "  VALUES (:completed, :started, :pilot_id, :aircraft_id, :zone_id)"
+      )
       .bind("completed", flight.isCompleted())
       .bind("started", flight.isStarted())
       .bind("pilot_id", flight.getPilotId())
       .bind("aircraft_id", flight.getAircraftId())
-      .bind("zone_id", flight.getZoneId()));
+      .bind("zone_id", flight.getZoneId())
+      .execute());
   }
 }
